6c63f9b01c295db51593f946b8ac30ddf84a095c
kolamat <matanmidupe004@gmail.com>
2024-04-25 19:08:14 +0100

    On branch master Your branch is ahead of 'origin/master' by 10 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/App.jsx
    	new file:   src/assets/plus&minus.svg
    	modified:   src/components/Accordion.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import FAQ from "./components/REUSABLES";
+import REUSABLE from "./components/REUSABLES";
 
 function App() {
   return (
-    <div className="w-full h-screen bg-gradient-to-r from-indigo-500 to-blue-600">
+    <div className="w-full h-screen bg-[#0c0c1a] text-gray-200">
       <div className="p-4">
-        <FAQ />
+        My Gees the number1 question is the only one for you guys not number 2 &
+        3
+        <REUSABLE />
       </div>
     </div>
   );
--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import plusMinus from "../assets/plus&minus.svg";
 
 const Accordion = ({ Title, Answer1, Answer2 }) => {
   const [accordionOpen, setAccordionOpen] = useState(false);
@@ -10,31 +11,7 @@ const Accordion = ({ Title, Answer1, Answer2 }) => {
       >
         <span>{Title}</span>
         {/* {accordionOpen ? <span>---</span> : <span>+</span>} */}
-        <svg
-          className="fill-indigo-500 shrink-0 ml-8"
-          width="16"
-          height="16"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect
-            y="7"
-            width="16"
-            height="2"
-            rx="1"
-            className={`transform origin-center transition duration-200 ease-out ${
-              accordionOpen && `!rotate-180`
-            }`}
-          />
-          <rect
-            y="7"
-            width="16"
-            height="2"
-            rx="1"
-            className={`transform origin-center rotate-90 transition duration-200 ease-out ${
-              accordionOpen && "!rotate-180"
-            }`}
-          />
-        </svg>
+        <plusMinus />
       </button>
       <div
         className={`grid overflow-hidden transition-all duration-300 ease-in-out text-slate-600 text-sm ${
