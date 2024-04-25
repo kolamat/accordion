208d1ec8003b788010e4395020698dcd8e102622
kolamat <matanmidupe004@gmail.com>
2024-04-25 18:09:26 +0100

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   src/components/Accordion.jsx
    	modified:   src/components/FAQ.jsx

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const Accordion = () => {
+const Accordion = ({ Title, Answer1, Answer2 }) => {
   const [accordionOpen, setAccordionOpen] = useState(false);
   return (
     <div>
@@ -8,8 +8,33 @@ const Accordion = () => {
         onClick={() => setAccordionOpen(!accordionOpen)}
         className={`flex justify-between w-full`}
       >
-        <span>This is The Title</span>
-        {accordionOpen ? <span>---</span> : <span>+</span>}
+        <span>{Title}</span>
+        {/* {accordionOpen ? <span>---</span> : <span>+</span>} */}
+        <svg
+          className="fill-indigo-500 shrink-0 ml-8"
+          width="16"
+          height="16"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            y="7"
+            width="16"
+            height="2"
+            rx="1"
+            className={`transform origin-center transition duration-200 ease-out ${
+              accordionOpen && `!rotate-180`
+            }`}
+          />
+          <rect
+            y="7"
+            width="16"
+            height="2"
+            rx="1"
+            className={`transform origin-center rotate-90 transition duration-200 ease-out ${
+              accordionOpen && "!rotate-180"
+            }`}
+          />
+        </svg>
       </button>
       <div
         className={`grid overflow-hidden transition-all duration-300 ease-in-out text-slate-600 text-sm ${
@@ -18,7 +43,8 @@ const Accordion = () => {
             : `grid-rows-[0fr] opacity-0`
         }`}
       >
-        <div className={`overflow-hidden`}>This Is The Anwser</div>
+        <div className={`overflow-hidden`}>{Answer1}</div>
+        <div className={`overflow-hidden`}>{Answer2}</div>
       </div>
     </div>
   );
--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -4,7 +4,17 @@ import Accordion from "./Accordion";
 const FAQ = () => {
   return (
     <div className="p-4 bg-gray-200 rounded-lg">
-      <Accordion />
+      <Accordion
+        Title="Do you want me to be your nigga??"
+        Answer1="No"
+        Answer2="Yes"
+      />
+      <Accordion
+        Title="Can I be your Boyfriend and later BoyLover??"
+        Answer1="No"
+        Answer2="Yes"
+      />
+      <Accordion Title="Will you Marry me??" Answer1="No" Answer2="Yes" />
     </div>
   );
 };
