a92898b4cac24d04b3fabe2aa74fdb0b2e773772
kolamat <matanmidupe004@gmail.com>
2024-04-25 17:36:48 +0100

    On branch master Your branch is ahead of 'origin/master' by 6 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/components/Accordion.jsx

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -4,9 +4,12 @@ const Accordion = () => {
   [accordionOpen, setAccordionOpen] = useState(false);
   return (
     <div>
-      <button className={`flex justify-between w-full`}>
+      <button
+        onClick={() => setAccordionOpen(!accordionOpen)}
+        className={`flex justify-between w-full`}
+      >
         <span>This is The Title</span>
-        <span>+</span>
+        {accordionOpen ? <span>-</span> : <span>+</span>}
       </button>
       <div
         className={`grid overflow-hidden transition-all duration-300 ease-in-out text-slate-600 text-sm ${
