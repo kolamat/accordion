a2298aa4d724755ee42f4b785479f12d153af4a5
kolamat <matanmidupe004@gmail.com>
2024-04-25 17:32:37 +0100

    On branch master Your branch is ahead of 'origin/master' by 5 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/components/Accordion.jsx

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = () => {
+  [accordionOpen, setAccordionOpen] = useState(false);
   return (
     <div>
-      <button className="flex justify-between w-full">
+      <button className={`flex justify-between w-full`}>
         <span>This is The Title</span>
         <span>+</span>
       </button>
+      <div
+        className={`grid overflow-hidden transition-all duration-300 ease-in-out text-slate-600 text-sm ${
+          accordionOpen
+            ? `grid-rows-[1fr] opacity-100`
+            : `grid-rows-[0fr] opacity-0`
+        }`}
+      >
+        <div className={`overflow-hidden`}>This Is The Anwser</div>
+      </div>
     </div>
   );
 };
