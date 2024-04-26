ea364afd3d93c77f5ec5760ef9fd2e03f045671e
kolamat <matanmidupe004@gmail.com>
2024-04-26 15:22:39 +0100

    On branch master Your branch is ahead of 'origin/master' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/components/Accordion.jsx

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -10,32 +10,7 @@ const Accordion = ({ Questions, Answer1, Answer2 }) => {
         className={`flex justify-between w-full text-left items-center`}
       >
         <div>{Questions}</div>
-        {/* {accordionOpen ? <span>---</span> : <span>+</span>} */}
-        <svg
-          className={`fill-black shrink-0 ml-8`}
-          width="16"
-          height="16"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect
-            y="7"
-            width="16"
-            height="2"
-            rx="1"
-            className={`transform origin-center transition duration-200 ease-out ${
-              accordionOpen && `!rotate-180`
-            }`}
-          />
-          <rect
-            y="7"
-            width="16"
-            height="2"
-            rx="1"
-            className={`transform origin-center rotate-90 transition duration-200 ease-out ${
-              accordionOpen && "!rotate-180"
-            }`}
-          />
-        </svg>
+        {accordionOpen ? <FaMinus /> : <FaPlus />}
       </button>
       <div
         className={`grid overflow-hidden transition-all duration-300 ease-in-out text-slate-600 text-sm ${
