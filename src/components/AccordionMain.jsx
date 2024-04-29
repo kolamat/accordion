cf17e79d522ed1bf37eada56919dbab2113029d8
kolamat <matanmidupe004@gmail.com>
2024-04-29 21:52:16 +0100

    On branch master Your branch is ahead of 'origin/master' by 9 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/components/AccordionMain.jsx

--- src/components/AccordionMain.jsx
+++ src/components/AccordionMain.jsx
@@ -3,7 +3,26 @@ import Accordion1, { AccordianItem } from "./accordion1";
 const AccordionMain = () => {
   return (
     <div>
-      <Accordian></Accordian>
+      <Accordion1 className="max-w-lg">
+        <AccordianItem value="1" trigger="Accordian item 1">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum,
+          recusandae? Dolor quod dolorum, nam nihil odio dicta at totam, quasi
+          ullam provident vero sit impedit? Consequatur accusamus eaque quo
+          quidem.
+        </AccordianItem>
+        <AccordianItem value="2" trigger="Accordian item 2">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum,
+          recusandae? Dolor quod dolorum, nam nihil odio dicta at totam, quasi
+          ullam provident vero sit impedit? Consequatur accusamus eaque quo
+          quidem.
+        </AccordianItem>
+        <AccordianItem value="3" trigger="Accordian item 3">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum,
+          recusandae? Dolor quod dolorum, nam nihil odio dicta at totam, quasi
+          ullam provident vero sit impedit? Consequatur accusamus eaque quo
+          quidem.
+        </AccordianItem>
+      </Accordion1>
     </div>
   );
 };
