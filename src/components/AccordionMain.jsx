415ab9b7b6deda578a1914ff03b6627083561f8f
kolamat <matanmidupe004@gmail.com>
2024-04-29 21:48:02 +0100

    On branch master Your branch is ahead of 'origin/master' by 8 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	new file:   src/components/AccordionMain.jsx

---
+++ src/components/AccordionMain.jsx
@@ -0,0 +1,11 @@
+import Accordion1, { AccordianItem } from "./accordion1";
+
+const AccordionMain = () => {
+  return (
+    <div>
+      <Accordian></Accordian>
+    </div>
+  );
+};
+
+export default AccordionMain;
